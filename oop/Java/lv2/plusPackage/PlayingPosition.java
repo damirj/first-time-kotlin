87193e98c167c667309521f47f553af9dbf5f7e8
Damir <d.jurkovic934@gmail.com>
2019-03-27 00:23:26 +0100

    pre-defined files

---
+++ oop/Java/lv2/plusPackage/PlayingPosition.java
@@ -0,0 +1,13 @@
+package plusPackage;
+
+/**
+ * 
+ * PlayingPosition enumeration with the following values: FW, MF, DF i GK; corresponding to 
+ * forward, midfielder, defender and goalkeeper.
+ * 
+ * @author OOPJ
+ *
+ */
+public enum PlayingPosition {
+	FW, MF, DF, GK
+}
